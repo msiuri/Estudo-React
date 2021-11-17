d950918bc06ed38c5acdf3aad7b3fa43ba087228
Iuri Moura <msiuri@outlook.com>
2021-11-16 22:52:17 -0300

    first commit

---
+++ src/templates/Home/index.jsx
@@ -0,0 +1,31 @@
+import "./App.css";
+import { Component } from "react";
+
+import { loadPosts } from "./utils/load-posts";
+import { Posts } from "./components/Posts";
+
+class App extends Component {
+  state = {
+    posts: [],
+  };
+
+  async componentDidMount() {
+    await this.loadPosts();
+  }
+  loadPosts = async () => {
+    const postsAndPhotos = await loadPosts();
+    this.setState({ posts: postsAndPhotos });
+  };
+
+  render() {
+    const { posts } = this.state;
+
+    return (
+      <section className="container">
+        <Posts posts={posts} />
+      </section>
+    );
+  }
+}
+
+export default App;
